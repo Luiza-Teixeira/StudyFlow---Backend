fbbdf02d6b3d2692eafc5f4d4618fe9b54294dc0
Arthur Azevedo <arthur.makets@gmail.com>
2026-06-08 12:02:16 +0000

    adiciona entidade Estudante e EstudanteRepository para o fluxo

---
+++ src/main/java/com/studyflow/repository/EstudanteRepository.java
@@ -0,0 +1,9 @@
+package com.studyflow.repository;
+
+import com.studyflow.entity.Estudante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstudanteRepository extends JpaRepository<Estudante, Long> {
+}
