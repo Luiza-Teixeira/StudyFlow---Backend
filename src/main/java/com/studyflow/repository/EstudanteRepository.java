f1134e52020858c848c59cea519729efac453749
Arthuracdpgit <arthur.makets@gmail.com>
2026-06-08 16:05:23 -0300

    query method de validacao em EstudanteRepository

--- src/main/java/com/studyflow/repository/EstudanteRepository.java
+++ src/main/java/com/studyflow/repository/EstudanteRepository.java
@@ -1,9 +1,14 @@
 package com.studyflow.repository;
 
-import com.studyflow.entity.Estudante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.studyflow.entity.Estudante;
+
 @Repository
 public interface EstudanteRepository extends JpaRepository<Estudante, Long> {
+
+
+    boolean existsByEmail(String email);
+    boolean existsByEmailAndIdNot(String email, Long id);
 }
